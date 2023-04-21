a40878c45359bb29fe33cf7df21245af6b1a695c
1AnthonyB1 <124379293+1AnthonyB1@users.noreply.github.com>
2023-04-21 13:44:49 +0000

    Created "Begin" Screen

--- Anthony.py
+++ Anthony.py
@@ -1,4 +1,18 @@
+#Introduction
 print()
 x = input("Hello There, I'm ... What's your name? ")
 print()
 print("Hello",x,"nice to meet you...")
+
+#The adventure begins
+while True:
+    y = input("Are you ready to begin? ")
+    if y == "no":
+        True
+    elif y == "yes":
+        False
+        break
+
+print()
+print("Alright then ",x,"...")
+print("You find yerself in a old sailing ship at sea \n You are unsure how you got there but all \n you know is that you need to get out of there")
