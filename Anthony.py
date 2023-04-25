ccec39118cec9a827fa5ae3be4df17a7b65e227c
1AnthonyB1 <124379293+1AnthonyB1@users.noreply.github.com>
2023-04-25 12:16:25 +0000

    The Adventure Begins

--- Anthony.py
+++ Anthony.py
@@ -6,12 +6,14 @@ print("Hello",x,"nice to meet you...")
 
 #The adventure begins
 while True:
-    y = input("Are you ready to begin? ")
-    if y == "no":
+    y = input("Are you ready to begin now? \nYes or No... ")
+    y = str.upper(y)
+    if y == "NO":
         True
-    elif y == "yes":
+        print("\nOk then... I guess you'll never witness this epic story...\n")
+    elif y == "YES":
         False
         break
 
 while True:
-    z = input("")
+    z = input("\nGreat! The adventure begins... \n")
